58f8a4c08ea7c84faf91313246da7e6ceb1530be
Pratika NG <pratika.05ng@gmail.com>
2025-09-10 18:28:29 +0530

    final commit

--- one_music_app/src/app/page.tsx
+++ one_music_app/src/app/page.tsx
@@ -1,5 +1,10 @@
 import FeaturedSection from "@/components/featuredSection";
+import Footer from "@/components/footer";
 import HeroSection from "@/components/heroSection";
+import Instructors from "@/components/instructor";
+import TestimonialCards from "@/components/testimonialCards";
+import UpcomingWebinars from "@/components/upcomingWebinars";
+import WhyChooseUsSection from "@/components/whyChooseUsSection";
 import Image from "next/image";
 
 export default function Home() {
@@ -10,6 +15,11 @@ export default function Home() {
     </h1>
     <HeroSection/>
     <FeaturedSection/>
+    <WhyChooseUsSection/>
+    <TestimonialCards/>
+    <UpcomingWebinars/>
+    <Instructors/>
+    <Footer/>
     </main>
   );
 }
